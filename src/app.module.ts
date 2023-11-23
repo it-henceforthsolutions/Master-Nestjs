fa837d79f7b4fcd80ab72ab56546b8abf79ec02f
mohini <mohini872601@gmail.com>
2023-11-23 12:02:02 +0530

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.DB_URL
+      }),
+    }),
+    ConfigModule.forRoot({isGlobal: true,}),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
