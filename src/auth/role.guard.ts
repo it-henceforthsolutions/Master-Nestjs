9ee50f1bb7986faa1696d831f33cd3304f75313b
mohini <mohini872601@gmail.com>
2023-12-04 18:12:34 +0530

    rolegurd

--- src/auth/role.guard.ts
+++ src/auth/role.guard.ts
@@ -6,24 +6,24 @@ import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector,private userService: UsersService) {}
+    constructor(private reflector: Reflector, private userService: UsersService) { }
 
-  async canActivate(context: ExecutionContext) {
-    const requiredRoles = this.reflector.getAllAndOverride<UsersType[]>(ROLES_KEY, [ 
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    
-    if (!requiredRoles) {
-        return true
+    async canActivate(context: ExecutionContext) {
+        const requiredRoles = this.reflector.getAllAndOverride<UsersType[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (!requiredRoles) {
+            return true
+        }
+        let user = context.switchToHttp().getRequest();
+        user = await this.userService.findUser(user.user.id)
+        let hasrequiredRoles = requiredRoles.some((role) => user.user_type?.includes(role));
+        if (!hasrequiredRoles) {
+            throw new HttpException('You have no permission to access this resource', HttpStatus.FORBIDDEN)
+        }
+        return hasrequiredRoles
+
     }
-    let user  = context.switchToHttp().getRequest();
-    user = await this.userService.findUser(user.user.id)
-    let hasrequiredRoles =  requiredRoles.some((role) => user.user_type?.includes(role));
-    if(!hasrequiredRoles){
-        throw new HttpException('You have no permission to access this resource',HttpStatus.FORBIDDEN)
-    }
-    return hasrequiredRoles
-    
-  }
 }
