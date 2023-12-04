ae25bf013c7f7abd9e1ec4b39c9abaac304b64b7
mohini <mohini872601@gmail.com>
2023-12-04 11:41:28 +0530

    summary added

--- src/quotes/quotes.service.ts
+++ src/quotes/quotes.service.ts
@@ -27,41 +27,43 @@ export class QuotesService {
 
     async findAll(searchQuery: PaginationDto) {
         try {
-                let startDate: any;
-                let endDate: any;
-                if (searchQuery.start_date && searchQuery.end_date) {
-                    let start_date = moment(searchQuery.start_date);
-                    startDate = start_date.startOf('day').valueOf(); // Use startOf('day') and valueOf() to get the start of the day in milliseconds
-                    let end_date = moment(searchQuery.end_date);
-                    endDate = end_date.endOf('day').valueOf(); // Use startOf('day') and valueOf() to get the start of the day in milliseconds
-                }
-                let query = {
-                    ...(searchQuery.search && { $or: [
+            let startDate: any;
+            let endDate: any;
+            if (searchQuery.start_date && searchQuery.end_date) {
+                let start_date = moment(searchQuery.start_date);
+                startDate = start_date.startOf('day').valueOf(); // Use startOf('day') and valueOf() to get the start of the day in milliseconds
+                let end_date = moment(searchQuery.end_date);
+                endDate = end_date.endOf('day').valueOf(); // Use startOf('day') and valueOf() to get the start of the day in milliseconds
+            }
+            let query = {
+                ...(searchQuery.search && {
+                    $or: [
                         { first_name: { $regex: searchQuery.search, $options: 'i' } },
                         { last_name: { $regex: searchQuery.search, $options: 'i' } },
                         { email: { $regex: searchQuery.search, $options: 'i' } },
-                    ] }),
-                    ...((searchQuery.start_date && searchQuery.end_date) && {
-                        created_at: {
-                            $gte: Number(startDate),
-                            $lte: Number(endDate)
-                        }
-                    }),
-                    is_deleted: false
-                }
-                let project :any
-                if(searchQuery.start_date && searchQuery.end_date){
-                    project = {
-                        first_name: 1,last_name : 1, _id: 1, is_active: 1,email: 1,country_code:1,phone_no:1 
+                    ]
+                }),
+                ...((searchQuery.start_date && searchQuery.end_date) && {
+                    created_at: {
+                        $gte: Number(startDate),
+                        $lte: Number(endDate)
                     }
+                }),
+                is_deleted: false
+            }
+            let project: any
+            if (searchQuery.start_date && searchQuery.end_date) {
+                project = {
+                    first_name: 1, last_name: 1, _id: 1, is_active: 1, email: 1, country_code: 1, phone_no: 1
                 }
-                let options = await this.commonService.set_options(searchQuery.pagination, searchQuery.limit)
-                let data = await this.quotes.find(query, project , options);
-                let count = await this.quotes.countDocuments(query)
-                return {
-                    data: data,
-                    count: count
-                }
+            }
+            let options = await this.commonService.set_options(searchQuery.pagination, searchQuery.limit)
+            let data = await this.quotes.find(query, project, options);
+            let count = await this.quotes.countDocuments(query)
+            return {
+                data: data,
+                count: count
+            }
         }
         catch (error) {
             throw new error
@@ -70,7 +72,11 @@ export class QuotesService {
 
     async findOne(id: string) {
         try {
-                return await this.quotes.findOne({_id: new Types.ObjectId(id),is_deleted:false});
+            let data = await this.quotes.findOne({ _id: new Types.ObjectId(id), is_deleted: false });
+            if(!data){
+                throw new HttpException('Deleted Quote',HttpStatus.BAD_REQUEST)
+            }
+            return data
         }
         catch (error) {
             throw error
@@ -80,12 +86,12 @@ export class QuotesService {
     async update(id: string) {
         try {
             let quote = await this.findOne(id)
-                if (quote.is_resolved == true) {
-                    await this.quotes.findByIdAndUpdate({ _id: id }, { is_resolved: false }, { new: true });
-                    throw new HttpException('Pending!!',HttpStatus.OK)
-                }
-                    await this.quotes.findByIdAndUpdate({ _id: id }, { is_resolved: true }, { new: true });
-                    throw new HttpException('Resolved!!',HttpStatus.OK)
+            if (quote.is_resolved == true) {
+                await this.quotes.findByIdAndUpdate({ _id: id }, { is_resolved: false }, { new: true });
+                throw new HttpException('Pending!!', HttpStatus.OK)
+            }
+            await this.quotes.findByIdAndUpdate({ _id: id }, { is_resolved: true }, { new: true });
+            throw new HttpException('Resolved!!', HttpStatus.OK)
         }
         catch (error) {
             console.log(error);
@@ -96,13 +102,13 @@ export class QuotesService {
     async remove(id: string) {
         try {
             await this.quotes.findOneAndUpdate(
-                {is_deleted:false,_id: new Types.ObjectId(id)},
-                {is_deleted:true},
-                {new:true}
+                { is_deleted: false, _id: new Types.ObjectId(id) },
+                { is_deleted: true },
+                { new: true }
             )
-            throw new HttpException('Deleted!!',HttpStatus.OK)
+            throw new HttpException('Deleted!!', HttpStatus.OK)
         } catch (error) {
-            throw new error
+            throw error
         }
     }
 }
