0c8843d9ceafb1fd5947f4db6e42b199b3a29529
Sumit Kumar <sumit.k.henceforth@gmail.com>
2024-08-17 13:25:49 +0530

    delivered & read

--- src/chat/dto/index.ts
+++ src/chat/dto/index.ts
@@ -1,17 +1,17 @@
 import {
     create_connection, join_connection, sendMessage, readMessage,
     deleteMessage, addGroupMember, mute_connection_skt, add_pin_items, deliver_message,
-    call_detail, join_stream, leave_stream
+    call_detail, join_stream, leave_stream, get_all_message
 } from "./chat"
 import {
     CreateGroupDto, AddGroupMemberDto, paginationsort, pagination,
     paginationsortsearch, mute_connection, block_unblock, chat_setting, start_call, join_call, get_pin_items,
-    list_connection, create_stream
+    list_connection, create_stream, Deliver_message
 } from "./chat2"
 
 export{
-    create_connection, join_connection, sendMessage, readMessage, deleteMessage, list_connection ,addGroupMember,
+    create_connection, join_connection, sendMessage, readMessage, deleteMessage, list_connection ,addGroupMember, get_all_message,
     CreateGroupDto, AddGroupMemberDto, paginationsort, pagination, paginationsortsearch, mute_connection, mute_connection_skt
     , add_pin_items, block_unblock, start_call, join_call, get_pin_items,
-    chat_setting, call_detail, deliver_message ,create_stream, join_stream, leave_stream
+    chat_setting, call_detail, deliver_message ,create_stream, join_stream, leave_stream, Deliver_message
 }
